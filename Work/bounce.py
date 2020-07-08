67cf5ccee7d902acd5999538682640e885279a53
Jacob_Newman <jen053@mun.ca>
2020-07-08 11:24:30 -0230

    Adding .py files from examples.

--- Work/bounce.py
+++ Work/bounce.py
@@ -1,3 +1,20 @@
 # bounce.py
-#
+# Program that shows the height of ball as it is dropped from 100 m and bounces 10 times. Each time the ball bounces
+# the height is reduced by 1/3 the previous height.
+
+
+import matplotlib.pyplot as plt
+height = 100
+bounces = 0
+
+plt.figure(figsize=(10, 8))
+plt.xlim(0, 5)
+while bounces < 10:
+    bounces = bounces + 1
+    height = height - 1/3 * height
+    print(height)
+    plt.scatter(2.5, height)
+    plt.text(2.6, height, 'bounce {}'.format(bounces))
+
+plt.show()
 # Exercise 1.5
--- Work/mortgage.py
+++ Work/mortgage.py
@@ -1,3 +1,27 @@
 # mortgage.py
-#
+principal = 500000.0
+rate = 0.05
+payment = 2684.11
+total_paid = 0
+month = 0
+extra_payment_start_month = 61
+extra_payment_end_month = 108
+extra_payment = 1000
+
+while principal > 0:
+    month = month + 1
+    principal = principal * (1 + rate / 12) - payment
+    total_paid = total_paid + payment
+
+    if (month >= extra_payment_start_month) and (month <= extra_payment_end_month):
+        principal = principal - extra_payment
+        total_paid = total_paid + extra_payment
+
+    if principal < 0:
+        total_paid = total_paid + principal
+        principal = 0
+
+    print(f'Month: {month}, Principal: ${principal:0.2f}, Total_paid: ${total_paid:0.2f}')
+
+
 # Exercise 1.7
