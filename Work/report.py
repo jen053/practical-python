9918903a3494f19a79a842b22bf0290026ea2f01
Jacob_Newman <jen053@mun.ca>
2020-07-15 10:57:17 -0230

    Modified program to read .csv of any format.

--- Work/report.py
+++ Work/report.py
@@ -1,41 +1,17 @@
 # report.py
 import csv
 
-
-def portfolio_value(filename):
-    """Function to calculate the total portfolio value.
-    Input of type string defining a file location.
-    File at file location of the form nx3 (stock name, stock price, total shares owned),
-    n being the number of stocks within the profile
-    """
-    f = open(filename)
-    rows = csv.reader(f)
-    headers = next(rows)
-    prices = []
-    shares = []
-    for row in rows:
-        prices.append(float(row[1]))
-        shares.append(float(row[2]))
-    prices_array = np.array(prices)
-    shares_array = np.array(shares)
-    values_array = prices_array * shares_array
-    total_value = np.sum(values_array)
-    print('Total portfolio valued at ${}'.format(total_value))
-    f.close()
-
-    return total_value
-
-
 def read_portfolio(filename):
     portfolio = []
-
     with open(filename, 'rt') as f:
         rows = csv.reader(f)
         headers = next(rows)
         for row in rows:
-            stock = dict(name=str(row[0]),
-                         shares=int(row[1]), price=float(row[2]))
+            record = dict(zip(headers, row))
+            stock = {'name': record['name'], 'shares': int(record['shares']),
+                     'price': float(record['price'])}
             portfolio.append(stock)
+
     return portfolio
 
 
@@ -52,7 +28,7 @@ def read_prices(filename_prices):
     return prices
 
 
-port = read_portfolio('Data/portfolio.csv')
+port = read_portfolio('Data/portfoliodate.csv')
 prices = read_prices('Data/prices.csv')
 
 total_cost = 0.0
