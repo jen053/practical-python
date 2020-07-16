465c7ba74737481e4f3866992d27f23a938c7a5a
Jacob_Newman <jen053@mun.ca>
2020-07-16 17:58:33 -0230

    Added comments to show location of exercises 3.3 - 3.7

--- Work/fileparse.py
+++ Work/fileparse.py
@@ -1,19 +1,21 @@
 # fileparse.py
 import csv
 
-
+# Exercise 3.3
 def parse_csv(filename, select=None, types=None, has_headers=True, delimiter=','):
     """
     Parse a CSV file into a list of  records
     """
 
     with open(filename) as f:
-
+	# Exercise 3.7
         rows = csv.reader(f, delimiter=delimiter)
 
+	# Exercise 3.6
         # Read the file headers
         headers = next(rows) if has_headers else []
-
+	
+	# Exercise 3.4
         # If a column selector was given, fine indices of the specified column.
         # Also narrow the set of headers used for resulting dictionaries.
         if select:
@@ -28,9 +30,12 @@ def parse_csv(filename, select=None, types=None, has_headers=True, delimiter=','
             # Filter the row if specific columns were selected
             if indices:
                 row = [row[index] for index in indices]
-            # Apply type conversion it types
+
+            # Exercise 3.5
+	    # Apply type conversion it types
             if types:
                 row = [func(val) for func, val in zip(types, row)]
+
             # Create dictionary if headers, else contain rows within tuples
             if headers:
                 record = dict(zip(headers, row))
