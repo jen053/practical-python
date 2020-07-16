c63404b61775ab7152d2489965d7ebd644d9f5cd
Jacob_Newman <jen053@mun.ca>
2020-07-16 09:47:03 -0230

    Program to read csv file. Can handle files with headers and no headers. Can convert elements of rows to defined types. Has column selection option.

--- Work/fileparse.py
+++ Work/fileparse.py
@@ -1,3 +1,42 @@
 # fileparse.py
-#
-# Exercise 3.3
+import csv
+
+
+def parse_csv(filename, select=None, types=None, has_headers=True):
+    """
+    Parse a CSV file into a list of  records
+    """
+
+    with open(filename) as f:
+        rows = csv.reader(f)
+
+        # Read the file headers
+        headers = next(rows) if has_headers else []
+
+        # If a column selector was given, fine indices of the specified column.
+        # Also narrow the set of headers used for resulting dictionaries.
+        if select:
+            indices = [headers.index(colname) for colname in select]
+            headers = select
+        else:
+            indices = []
+
+        records = []
+        for row in rows:
+            if not row:
+                continue  # Skip rows with no data
+            # Filter the row if specific columns were selected
+            if indices:
+                row = [row[index] for index in indices]
+            # Apply type conversion it types
+            if types:
+                row = [func(val) for func, val in zip(types, row)]
+            # Create dictionary if headers, else contain rows within tuples
+            if headers:
+                record = dict(zip(headers, row))
+            else:
+                records = tuple(row)
+
+            records.append(record)
+
+    return records
