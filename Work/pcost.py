96041c3b202d1b09afbc5084c43d963988356d04
Jacob_Newman <jen053@mun.ca>
2020-07-17 15:04:35 -0230

    Exercise 3.14: Modified to use report.read_portfolio to import and read data.

--- Work/pcost.py
+++ Work/pcost.py
@@ -1,7 +1,7 @@
 # pcost.py
-import os
 import csv
-import numpy as np
+# Exercise 3.14
+import report
 
 
 def portfolio_value(filename):
@@ -9,31 +9,13 @@ def portfolio_value(filename):
     Function to calculate the total portfolio value.
     Input of type string defining a file location.
     File at file location of the form nx3 (stock name,
-    stock price, total shares owned),                                                                                                                              n being the number of stocks within the profile
-	"""
+    stock price, total shares owned),
+    """
 
-    f = open(filename)
-    rows = csv.reader(f)
-    headers = next(rows)
-    total_cost = 0.0
-    record = []
-    for rowno, row in enumerate(rows, start=1):
-        r = dict(zip(headers, row))
-        try:
-            nshares = int(r['shares'])
-            price = float(r['price'])
-            total_cost += nshares * price
-        except ValueError:
-            print(f'Row {rowno}: Bad row: {row}')
-        record.append(r)
-    print('Total portfolio valued at ${}'.format(total_cost))
-    f.close()
-
-    return total_cost, record
+    port = report.read_portfolio(filename)
+    return sum([s['shares'] * s['price'] for s in port])
 
 
-cost, record = portfolio_value('Data/portfoliodate.csv')
-
+cost = portfolio_value('Data/portfoliodate.csv')
 print(cost)
-print(record)
 # Exercise 1.27
